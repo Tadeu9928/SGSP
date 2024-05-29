a9c1284d29c0e7bab51ed51dd6eb6fded2add49c
Jonathas Menezes <jonathassk8@gmail.com>
2024-05-29 19:57:35 -0300

    project SGSP

---
+++ src/main/java/com/sgspproject/sgsp/Sgsp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sgspproject.sgsp;
+
+/**
+ *
+ * @author jonat
+ */
+public class Sgsp {
+
+    public static void main(String[] args) {
+        //iniciando o programa
+        System.out.println("Hello World!");
+    }
+}
